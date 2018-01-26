408773e0c103b76ef9da50e9cccafedc30e8e088
Fabpk90 <fabrizio1996@live.fr>
2018-01-26 23:18:16 +0100

    Adding ZombiePossesion

---
+++ Assets/Scripts/Zombie/Zombie.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInput))]
+public class Zombie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (transform.tag != "Player")
+            GameManager.AddZombie(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TakePossesion()
+    {
+        GetComponent<PlayerInput>().lastTimePossesed = Time.time;
+        GetComponent<PlayerInput>().enabled = true;
+    }
+}
