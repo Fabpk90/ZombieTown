dd6f01e1ceea4a78c63061135716b21733b7101e
Fabpk90 <fabrizio1996@live.fr>
2018-01-27 16:46:07 +0100

    Adding FMOD

---
+++ Assets/TestFMOD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFMOD : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/sfx_zombie_call");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
